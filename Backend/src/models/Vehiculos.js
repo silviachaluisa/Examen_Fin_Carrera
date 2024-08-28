f11844ac7fde03ecc247fe968ab71c0696787177
silviachaluisa <veronica-14-1998@hotmail.com>
2024-08-27 22:26:09 -0500

    modelo vehiculo

---
+++ Backend/src/models/Vehiculos.js
@@ -0,0 +1,57 @@
+import mongoose, { Schema,model } from "mongoose";
+import bcrypt from "bcryptjs"
+
+const vehiculoSchema=new Schema({
+
+    marca:{
+        type:String,
+        require:true,
+        trim:true
+    },
+    modelo:{
+        type:String,
+        require:true,
+        trim:true,
+    },
+    anio_fabricacion:{
+        type:Number,
+        require:true,
+        trim:true,  
+    },
+    placa:{
+        type:Number,
+        require:true,
+        trim:true, 
+        unique:true
+
+    },
+    color:{
+        type:String,
+        require:true,
+        trim:true
+
+    },
+    tipo_vehiculo:{
+        type:String,
+        require:true,
+        trim:true
+
+    },
+    kilometraje:{
+        type:Number,
+        require:true,
+        trim:true 
+
+    },
+    descripcion:{
+        type:String,
+        require:true,
+        trim:true
+
+    }
+
+},{timestamps:true
+    
+})
+
+export default model('model',vehiculoSchema)
